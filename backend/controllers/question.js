425360ce261142663574e37b15b06354bf29ffe4
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-08-23 14:52:17 +0530

    adding options to question

--- backend/controllers/question.js
+++ backend/controllers/question.js
@@ -4,7 +4,7 @@ const Form = require("../models/form");
 
 exports.addQuestion = async (req, res) => {
   try {
-    const { title, questionType } = req.body;
+    const { title } = req.body;
     const form = await Form.findById(req.params.formId);
     if (!form)
       return res.status(404).json({
@@ -26,14 +26,74 @@ exports.addQuestion = async (req, res) => {
     }
     const newQuestion = new Question();
     newQuestion.title = title;
-    newQuestion.questionType = questionType;
+    newQuestion.questionType = "mcq";
     newQuestion.form = form._id;
     await newQuestion.save();
+    form.questions.push(newQuestion._id);
+    await form.save();
     return res.status(200).json({
       success: true,
       message: "Question added",
       newQuestion,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.addOptions = async (req, res) => {
+  try {
+    const { questionId, option } = req.body;
+    const { formId } = req.params;
+    const quesiton = await Question.findById(questionId);
+    if (!quesiton)
+      return res.status(404).json({
+        success: false,
+        error: "Question not found",
+      });
+    const form = await Form.findById(formId);
+    if (!form)
+      return res.status(404).json({
+        success: false,
+        error: "Form not found",
+      });
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+    const isValidQuestion = form.questions.includes(quesiton._id);
+    if (!isValidQuestion) {
+      return res.status(404).json({
+        success: false,
+        error: "Question not found",
+      });
+    }
+    if (quesiton.questionType !== "mcq") {
+      return res.status(400).json({
+        success: false,
+        error: "Question is not mcq type",
+      });
+    }
+    // if (form.owner === user._id) {
+    quesiton.options.push(option);
+    await quesiton.save();
+    return res.status(200).json({
+      success: true,
+      message: "Option added",
+      quesiton,
+    });
+    // } else {
+    //   return res.status(401).json({
+    //     success: false,
+    //     error: "Unauthorized access",
+    //   });
+    // }
   } catch (error) {
     return res.status(500).json({
       success: false,
--- backend/routes/v1/question.js
+++ backend/routes/v1/question.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
-const { addQuestion } = require("../../controllers/question");
+const { addQuestion, addOptions } = require("../../controllers/question");
 
 const { isAuthenticated } = require("../../middleware/auth");
 
 router.post("/new-question/:formId", isAuthenticated, addQuestion);
+router.post("/add-option/:formId", isAuthenticated, addOptions);
 
 module.exports = router;
