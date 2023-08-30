f22eeab12aedede25b9f21493232cf0d5b66834f
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-08-30 09:57:12 +0530

    rendering component based on form owner

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import FormData from "form-data";
 import { useParams } from "react-router-dom";
 
 import { formByIdApi, updateTitleApi } from "../api/form";
 
+import { AuthContext } from "../context/AuthProvider";
+import FillForm from "../components/FillForm";
+
 const Form = () => {
+  const { user } = useContext(AuthContext);
+  const [isFormOwner, setIsFormOwner] = useState(false);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const { id } = useParams();
@@ -15,6 +20,10 @@ const Form = () => {
       setTitle(result.form.title);
       setDesc(result.form.description);
     }
+  };
+  const validateFormOwner = () => {
+    const isOwner = user.forms.includes(id);
+    isOwner ? setIsFormOwner(true) : setIsFormOwner(false);
   };
   const updateTitle = async (e) => {
     // setTitle(e.target.value);
@@ -30,29 +39,34 @@ const Form = () => {
   };
   useEffect(() => {
     getFormDetails();
+    validateFormOwner();
   }, []);
   return (
     <div>
-      <div className=" w-[40%] mt-10 border border-t-[#673ab6] border-t-8 rounded-md ">
-        <div className="mb-10">
-          <input
-            type="text"
-            className="focus:border-b-4 focus:border-b-[#673ab6] focus:outline-none text-2xl font-extrabold"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            onBlur={updateTitle}
-            width="100%"
-          />
+      {isFormOwner ? (
+        <div className=" w-[40%] mt-10 border border-t-[#673ab6] border-t-8 rounded-md ">
+          <div className="mb-10">
+            <input
+              type="text"
+              className="focus:border-b-4 focus:border-b-[#673ab6] focus:outline-none text-2xl font-extrabold"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              onBlur={updateTitle}
+              width="100%"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="focus:outline-none focus:border-b-[#673ab6] focus:border-b-2"
+              value={desc}
+              onChange={updateDescription}
+            />
+          </div>
         </div>
-        <div>
-          <input
-            type="text"
-            className="focus:outline-none focus:border-b-[#673ab6] focus:border-b-2"
-            value={desc}
-            onChange={updateDescription}
-          />
-        </div>
-      </div>
+      ) : (
+        <FillForm />
+      )}
     </div>
   );
 };
